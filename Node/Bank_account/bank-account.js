8ff9eef4f99accd14180ad3a02f305d8b8bacb2d
Alex Watson <Awatson347@aol.com>
2017-05-23 23:08:24 -0400

    5_23

---
+++ Node/Bank_account/bank-account.js
@@ -0,0 +1,23 @@
+var account = {
+	balance: 0
+};
+
+//deposit(account, amount)
+function deposit (account, amount){
+	account.balance+=amount;
+}
+
+//withdraw(account, amount)
+function withdraw (account, amount) {
+	account.balance-=amount;
+}
+
+//getBalance(account)
+
+function getBalance(account){
+	return account.balance;
+}
+deposit(account, 14000);
+console.log(getBalance(account));
+withdraw(account, 200)
+console.log(getBalance(account));
