f9af3338643d96908cd1ef4cde40f6422c87f7b6
Alex Watson <Awatson347@aol.com>
2017-06-14 00:38:59 -0400

    6-13

--- Node/password-manager/app.js
+++ Node/password-manager/app.js
@@ -1,19 +1,70 @@
 console.log('Starting password manager');
+
 var storage = require('node-persist');
 storage.initSync();
+
+var argv = require('yargs')
+	.command('create', 'Create a new account', function(yargs) {
+		yargs.options({
+			name: {
+				demand: true,
+				alias: 'n',
+				description: 'Account name (eg: Twitter, Facebook)',
+				type:'string'
+			},
+			username:{
+				demand:true,
+				alias:'u',
+				description:'Account username or email',
+				type:'string'
+			},
+			password:{
+				demand:true,
+				alias:'p',
+				description:'Account password',
+				type:'string'
+			}
+		}).help("help");
+	})
+	.command('get', 'get an existing account', function(yargs){
+		yargs.options({
+			name:{
+				demand: true,
+				alias:'n',
+				description:'Account name (e.g.Twitter, Facebook)',
+				type:'string'
+			}
+		}).help('help');
+	})
+	.help('help')
+	.argv;
+	var command=argv._[0];
+
+// create
+//     --name
+//     --username
+//     --password
+
+// get
+//     --name
+
 // account.name Facebook
-//account.username User12!
-//account.username Password
+// account.username User12!
+// account.password Password123!
+
 function createAccount (account) {
 var accounts = storage.getItemSync('accounts');
+	
 	if (typeof accounts === 'undefined') {
 		accounts = [];
 	}
+
 	accounts.push(account);
 	storage.setItemSync('accounts', accounts);
 
 	return account;
 }
+
 function getAccount (accountName) {
 var accounts = storage.getItemSync('accounts');
 var matchedAccount;
---
+++ Node/password-manager/example-JSON.js
@@ -0,0 +1,8 @@
+var person = {
+	name: 'Alex',
+	age:'47'
+};
+var personJSON = JSON.stringify(person);
+
+console.log(personJSON);
+console.log(typeof personJSON);
