cf205eb79c003895af1acf07ce0fde8b039531db
Alex Watson <Awatson347@aol.com>
2017-06-18 15:37:33 -0400

    6_18

--- Node/password-manager/example-JSON.js
+++ Node/password-manager/example-JSON.js
@@ -6,3 +6,19 @@ var personJSON = JSON.stringify(person);
 
 console.log(personJSON);
 console.log(typeof personJSON);
+
+var personObject = JSON.parse(personJSON);
+console.log(personObject.name);
+console.log(typeof personObject);
+
+console.log('CHALLENGE AREA');
+var animal =  '{"name": "Atrain"}';
+var animalObject = JSON.parse(animal);
+animalObject.age = 1;
+animal = JSON.stringify(animalObject);
+console.log(animal);
+
+
+
+
+
